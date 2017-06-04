ef50cc44036590ee53798386254e6f9b4025228f
tongchen <839706407@qq.com>
2017-06-04 15:57:46 +0800

    修复无缝更换皮肤失败的问题

--- app/src/main/java/com/tongchen/ganhuojizhongying/activity/MainActivity.java
+++ app/src/main/java/com/tongchen/ganhuojizhongying/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tongchen.ganhuojizhongying.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 import com.tongchen.ganhuojizhongying.R;
 
+import solid.ren.skinlibrary.SkinConfig;
 import solid.ren.skinlibrary.SkinLoaderListener;
 import solid.ren.skinlibrary.base.SkinBaseActivity;
 import solid.ren.skinlibrary.loader.SkinManager;
@@ -30,14 +33,19 @@ public class MainActivity extends SkinBaseActivity {
     }
 
     private void initViews() {
-        tabLyt = (TabLayout) findViewById(R.id.tabLyt_column);
-        //  使TabLayout的指示器支持换肤
-        dynamicAddView(tabLyt, "tabLayoutIndicator", R.color.colorPrimaryDark);
+        CoordinatorLayout corLyt=(CoordinatorLayout) findViewById(R.id.coorLyt);
+        //dynamicAddView(corLyt, "background", R.color.colorPrimaryDark);
 
-        //CoordinatorLayout cor=(CoordinatorLayout) findViewById(R.id.coorLyt);
+        AppBarLayout appBarLyt=(AppBarLayout) findViewById(R.id.appbarLyt);
+        //dynamicAddView(appBarLyt, "background", R.color.colorPrimaryDark);
+
         Toolbar to=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(to);
         dynamicAddView(to, "background", R.color.colorPrimaryDark);
+
+        tabLyt = (TabLayout) findViewById(R.id.tabLyt_column);
+        //  使TabLayout的指示器支持换肤
+        //dynamicAddView(tabLyt, "tabLayoutIndicator", R.color.colorPrimaryDark);
 
        /* tabLyt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,30 +56,35 @@ public class MainActivity extends SkinBaseActivity {
     }
 
     public void replaceSkin(View view) {
-        SkinManager.getInstance().loadSkin("theme.skin", new SkinLoaderListener() {
-                    @Override
-                    public void onStart() {
-                        Toast.makeText(getApplicationContext(), "正在切换中", Toast.LENGTH_SHORT).show();
+        if(SkinConfig.isDefaultSkin(MainActivity.this)){
+            SkinManager.getInstance().loadSkin("theme.skin", new SkinLoaderListener() {
+                        @Override
+                        public void onStart() {
+                            Toast.makeText(getApplicationContext(), "正在切换中", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onSuccess() {
+                            Toast.makeText(getApplicationContext(), "切换成功", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onFailed(String errMsg) {
+                            Toast.makeText(getApplicationContext(), "切换失败", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "onFailed: "+errMsg);
+                        }
+
+                        @Override
+                        public void onProgress(int progress) {
+
+                        }
                     }
+            );
+        }else{
+            SkinManager.getInstance().restoreDefaultTheme();
+        }
 
-                    @Override
-                    public void onSuccess() {
-                        Toast.makeText(getApplicationContext(), "切换成功", Toast.LENGTH_SHORT).show();
-                    }
 
-                    @Override
-                    public void onFailed(String errMsg) {
-                        Toast.makeText(getApplicationContext(), "切换失败", Toast.LENGTH_SHORT).show();
-                        Log.d(TAG, "onFailed: "+errMsg);
-                    }
-
-                    @Override
-                    public void onProgress(int progress) {
-
-                    }
-                }
-
-        );
     }
 
 
