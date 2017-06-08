7eb492c5150f9e712c9d5a86d16ea52291d7873c
TongChen <839706407@qq.com>
2017-06-08 12:02:28 +0800

    完成下拉刷新和上滑加载更多

---
+++ app/src/main/java/com/tongchen/ganhuojizhongying/constant/LoadingType.java
@@ -0,0 +1,24 @@
+package com.tongchen.ganhuojizhongying.constant;
+
+/**
+ * Created by TongChen on 2017/6/8.
+ * <p>
+ * Description:加载数据的类型
+ */
+
+public class LoadingType {
+
+    /**
+     * 进入后第一次加载，初始化数据
+     */
+    public static final int TYPE_INIT = 0;
+    /**
+     * 下拉刷新数据
+     */
+    public static final int TYPE_REFRESH = 1;
+    /**
+     * 上滑加载更多数据
+     */
+    public static final int TYPE_MORE = 2;
+
+}
--- app/src/main/java/com/tongchen/ganhuojizhongying/fragment/NewsFragment.java
+++ app/src/main/java/com/tongchen/ganhuojizhongying/fragment/NewsFragment.java
@@ -4,20 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tongchen.ganhuojizhongying.R;
 import com.tongchen.ganhuojizhongying.adapter.NewsAdapter;
+import com.tongchen.ganhuojizhongying.constant.LoadingType;
 import com.tongchen.ganhuojizhongying.constant.Url;
 import com.tongchen.ganhuojizhongying.gson.GanHuo;
+import com.tongchen.ganhuojizhongying.gson.Result;
 import com.tongchen.ganhuojizhongying.util.HttpUtil;
 import com.tongchen.ganhuojizhongying.util.UtilityUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -28,9 +35,18 @@ public class NewsFragment extends Fragment {
     private static final String ARG_TITLE = "title";
     //  当前所处Tab的分类
     private String mTitle;
+    private int loadedPageNum;
+    private int lastVisiblePosition;
+
 
     private View view;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
+    private List<Result> resultList = new ArrayList<>();
+
+    private NewsAdapter newsAdapter;
 
     public NewsFragment() {
 
@@ -63,18 +79,65 @@ public class NewsFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         findViews();
-        loadContent();
+        loadContent(LoadingType.TYPE_INIT);
     }
 
     private void findViews() {
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipe_refresh_lyt);
+
+        swipeRefreshLayout.setProgressBackgroundColorSchemeResource(android.R.color.white);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light,
+                android.R.color.holo_orange_light, android.R.color.holo_green_light);
+        swipeRefreshLayout.setProgressViewOffset(false, 0, (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 24, getResources().getDisplayMetrics()));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadContent(LoadingType.TYPE_REFRESH);
+                newsAdapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
         recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (lastVisiblePosition + 1 == newsAdapter.getItemCount()) {
+                    loadContent(LoadingType.TYPE_MORE);
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                lastVisiblePosition = linearLayoutManager.findLastVisibleItemPosition();
+                Log.d("Nsize","3---"+lastVisiblePosition);
+            }
+        });
     }
 
-    private void loadContent() {
-        String address = Url.GANHUO_DATA + mTitle + "/10/1";
+    private void loadContent(final int loadingType) {
+        String address;
+        switch (loadingType) {
+            case LoadingType.TYPE_INIT:
+            case LoadingType.TYPE_REFRESH:
+                loadedPageNum = 1;
+                address = Url.GANHUO_DATA + mTitle + "/10/" + loadedPageNum;
+                break;
+            case LoadingType.TYPE_MORE:
+                loadedPageNum++;
+                address = Url.GANHUO_DATA + mTitle + "/10/" + loadedPageNum;
+                break;
+            default:
+                address = Url.GANHUO_DATA + mTitle + "/10/1";
+                break;
+        }
         HttpUtil.sendOkHttpRequest(address, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -84,14 +147,32 @@ public class NewsFragment extends Fragment {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                String responseText = response.body().string();
+                final String responseText = response.body().string();
                 final GanHuo ganHuo = UtilityUtil.handleGanHuoResponse(responseText);
                 if (ganHuo != null) {
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            NewsAdapter newsAdapter = new NewsAdapter(ganHuo.getResults());
-                            recyclerView.setAdapter(newsAdapter);
+                            switch (loadingType) {
+                                case LoadingType.TYPE_INIT:
+                                    resultList.clear();
+                                    resultList.addAll(ganHuo.getResults());
+                                    newsAdapter = new NewsAdapter(resultList);
+                                    recyclerView.setAdapter(newsAdapter);
+                                    break;
+                                case LoadingType.TYPE_REFRESH:
+                                    resultList.clear();
+                                    resultList.addAll(ganHuo.getResults());
+                                    newsAdapter.notifyDataSetChanged();
+                                    swipeRefreshLayout.setRefreshing(false);
+                                    break;
+                                case LoadingType.TYPE_MORE:
+                                    Log.d("Nsize", "0---" + resultList.size());
+                                    resultList.addAll(ganHuo.getResults());
+                                    Log.d("Nsize", "1---" + resultList.size());
+                                    newsAdapter.notifyDataSetChanged();
+                                    break;
+                            }
                         }
                     });
                 }
